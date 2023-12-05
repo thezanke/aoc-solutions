304bae62e6de15e7f421fa74d35e55f222c4bbdc
Alex Howard <thezanke@gmail.com>
2023-12-05 17:15:18 -0500

    typings

--- 2023/day5/__main__.py
+++ 2023/day5/__main__.py
@@ -1,36 +1,49 @@
 import re
 import sys
-from common import chunk_list, expectation
 from timeit import default_timer as timer
+from typing import Any, List, Optional, Tuple
+from common import chunk_list, expectation
 
 debugging = False
 
 
-def debug_log(*args: object):
+from typing import Any, Optional, IO
+
+
+def debug_log(
+    *args: Any,
+    sep: Optional[str] = " ",
+    end: Optional[str] = "\n",
+    file: Optional[IO[str]] = None,
+    flush: bool = False
+):
     global debugging
+
     if debugging:
-        print(*args)
+        print(
+            *args,
+            sep=sep,
+            end=end,
+            file=file,
+            flush=flush,
+        )
 
 
-CATEGORIES = [
-    "seed",
-    "soil",
-    "fert",
-    "water",
-    "light",
-    "temp",
-    "humidity",
-    "location",
-]
+CATEGORIES = ["seed", "soil", "fert", "water", "light", "temp", "humidity", "location"]
+
+MapList = List[tuple[int, int, int, int]]
+
+Almanac = List[MapList]
+ParsedInput = Tuple[list[int], Almanac]
 
 
 def split(x: str):
     return [int(n) for n in re.split(r"\s+", x)]
 
 
-def get_input(filename: str):
+def get_input(filename: str) -> ParsedInput:
     with open(filename, "r") as file:
-        data: list[list[tuple[int, int, int]]] = []
+        data: list[MapList] = []
         head, *lines = file.read().splitlines()
 
         for line in lines:
@@ -42,13 +55,14 @@ def get_input(filename: str):
                 continue
 
             dest, start, range_len = split(line)
-            data[-1] += [(start, start + range_len - 1, dest)]
+            data[-1] += [(start, start + range_len - 1, dest, dest + range_len - 1)]
 
         return split(head[7:]), data
 
 
-def part1(inpt: tuple[list[int], list[list[tuple[int, int, int]]]]):
+def part1(inpt: ParsedInput):
     seeds, almanac = inpt
+
     debug_log("almanac: %s" % almanac)
     debug_log("seeds: %r" % seeds)
 
@@ -58,7 +72,7 @@ def part1(inpt: tuple[list[int], list[list[tuple[int, int, int]]]]):
         debug_log("\nseed: %s" % num)
 
         for category in range(len(CATEGORIES) - 1):
-            for start, end, dest in almanac[category]:
+            for start, end, dest, _ in almanac[category]:
                 if start <= num <= end:
                     debug_log("  ! found %r in %r" % (num, (start, end, dest)))
 
@@ -75,20 +89,63 @@ def part1(inpt: tuple[list[int], list[list[tuple[int, int, int]]]]):
     return lowest
 
 
+def convert_to_next_category(
+    input_list: MapList,
+    output_map: MapList,
+):
+    # output_list: MapList = []
+
+    for inpt in input_list:
+        debug_log(inpt)
+        for outpt in output_map:
+            overlaps = max(inpt[2], outpt[3]) <= min(inpt[1], outpt[1])
+            if overlaps:
+                debug_log(
+                    "  ! found %r in %r" % ((inpt[2], inpt[3]), (outpt[0], outpt[1]))
+                )
+
+    return input_list
+
+
+def generate_seed_to_location_map(almanac: Almanac):
+    seed_to_location: MapList = almanac[0].copy()
+
+    for dest_cat_id in range(1, len(CATEGORIES) - 1):
+        src_cat_id = dest_cat_id - 1
+
+        debug_log(
+            "%s %s -> %s %s"
+            % (
+                src_cat_id,
+                CATEGORIES[src_cat_id],
+                dest_cat_id,
+                CATEGORIES[dest_cat_id],
+            )
+        )
+
+        seed_to_location = convert_to_next_category(
+            seed_to_location,
+            almanac[dest_cat_id],
+        )
+
+    return seed_to_location
+
+
 # New Idea.. what if I work backwards.... pre-cache the ranges from each category to the one before it;
 # then, once we reach seed, it should be easier to determine the final value quickly. TBD..
-def part2(inpt: tuple[list[int], list[list[tuple[int, int, int]]]]):
-    global debugging
-
+def part2(inpt: ParsedInput):
     timer_start = timer()
 
     seeds, almanac = inpt
     debug_log("almanac: %s" % almanac)
     debug_log("seeds: %r" % seeds)
 
+    seed_to_location = generate_seed_to_location_map(almanac)
+    debug_log("seed_to_location: %r" % seed_to_location)
+
     lowest = sys.maxsize
     i = 0
-    chunks = chunk_list(seeds, 2)
+    chunks: list[list[int]] = chunk_list(seeds, 2)
     total = sum(chunk[1] for chunk in chunks)
 
     for start, size in chunks:
@@ -96,7 +153,7 @@ def part2(inpt: tuple[list[int], list[list[tuple[int, int, int]]]]):
             debug_log("\nseed: %s" % num)
 
             for category in range(len(CATEGORIES) - 1):
-                for start, end, dest in almanac[category]:
+                for start, end, dest, _ in almanac[category]:
                     if start <= num <= end:
                         debug_log("  ! found %r in %r" % (num, (start, end, dest)))
 
@@ -110,11 +167,13 @@ def part2(inpt: tuple[list[int], list[list[tuple[int, int, int]]]]):
 
             i += 1
 
-            timer_end = timer()
-            elapsed = timer_end - timer_start
+            elapsed = timer() - timer_start
             if i % 1000 == 0:
                 print(
-                    "%.4f" % (i / total) + "% complete in " + ("%.1f" % elapsed) + "s",
+                    ("%.4f" % (i / total))
+                    + "% complete in "
+                    + ("%.1f" % elapsed)
+                    + " sec",
                     end="\r",
                 )
 
