bd552440f1ac2fe1837f9d2708de1bbc80705493
TheZanke <thezanke@gmail.com>
2024-12-04 09:14:57 -0500

    starting part1

---
+++ 2024/day4/part1.py
@@ -0,0 +1,22 @@
+example = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"""
+
+def solve(data: str):
+  
+  pass
+
+if __name__ == "__main__":
+    example_results = solve(example)
+    assert example_results == 0, "Expected 0, got " + str(example_results)
+    print("✔️  All tests passed")
+
+    # results = solve(final)
+    # print("Day 4 Part 1:", results)
